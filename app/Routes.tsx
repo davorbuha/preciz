9ca4373b638ffa07fae890394ed2561d999ca8cb
marta1511 <50339660+marta1511@users.noreply.github.com>
2020-05-29 18:13:59 +0200

    reading values

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -5,9 +5,16 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 import SerialPort from 'serialport';
+const Readline = require('@serialport/parser-readline')
 
 export default function Routes() {
-  SerialPort.list().then(console.log);
+  SerialPort.list().then((res) => {console.log('ovdjeeeeeee', res);
+  const port = new SerialPort(res[0].path, { baudRate: 9600 })
+  const parser = new Readline()
+  port.pipe(parser)
+  parser.on('data', line => console.log(`> ${line}`))
+  });
+  
   return (
     <App>
       <Switch>
