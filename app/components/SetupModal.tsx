27b32759a8662d7b53025c58b8f80072f5eb1669
marta1511 <50339660+marta1511@users.noreply.github.com>
2020-06-05 20:52:58 +0200

    setup modal

--- app/components/SetupModal.tsx
+++ app/components/SetupModal.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { makeStyles, Modal } from '@material-ui/core';
+import { makeStyles, Modal, Button } from '@material-ui/core';
 import { useForm, Controller } from 'react-hook-form';
 import { TextField } from '@material-ui/core';
 import useWindowDimensions from '../useDimensions';
@@ -8,7 +8,7 @@ import colors from '../styles/colors';
 
 function renderErrorForField(errors: any, fieldName: string) {
   if (errors[fieldName]) {
-    return <p>{errors[fieldName].message}</p>;
+    return <p style={{fontSize: 12, color: 'red'}}>{errors[fieldName].message}</p>;
   }
   return null;
 }
@@ -16,25 +16,88 @@ function renderErrorForField(errors: any, fieldName: string) {
 const useStyles = makeStyles(theme => ({
   paper: ({ width, height }: any) => ({
     display: 'flex',
+    flexDirection: 'column',
     position: 'absolute',
     backgroundColor: colors.grey,
     boxShadow: theme.shadows[5],
     borderRadius: '5px',
     alignItems: 'center',
-    justifyContent: 'center',
-    width: width / 2,
-    height: height / 2,
-    top: height / 4,
-    left: width / 4
+    justifyContent: 'space-between',
+    padding: 32,
+    width: ((width / 2) - 32) + 'px',
+    height: ((height / 2)-32) + 'px',
+    top: ((height / 4)-32) + 'px',
+    left: ((width / 4)-32) + 'px'
   }),
+  textfieldWrapperFullWidth:{
+    width: '100%'
+  },
   textfieldWrapper: {
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    width: '45%'
+  },
+  inputRowWrapper: {
+    marginTop: '10px',
+    display: "flex",
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  wrapper: {
+    width: '80%'
   }
 }));
 
 interface Props {
   showSetupModal: boolean;
+}
+
+const rules = {
+  CompanyName: {
+    required: true,
+    minLength: {
+      value: 2,
+      message: "Ime tvrtke je pre kratko"
+    },
+    maxLength: {
+      value: 100,
+      message: "Ime tvrtke je pre dugačko"
+    }
+  },
+  CompanyAddress: {
+    required: true,
+    minLength: {
+      value: 2,
+      message: "Adresa tvrtke je pre kratka"
+    },
+    maxLength: {
+      value: 100,
+      message: "Adresa tvrtke je pre dugačka"
+    }
+  },
+  Oib: {
+    minLength: {
+      value: 11,
+      message: "OIB mora sadrzavati 11 znakova"
+    },
+    maxLength: {
+      value: 11,
+      message: "OIB mora sadrzavati 11 znakova"
+    }
+  },
+  Telephone: {
+    maxLength: {
+      value: 100,
+      message: "Broj Telefona je pre dugacak"
+    }
+  },
+  Email: {
+    maxLength: {
+      value: 100,
+      message: "Email je pre dugacak"
+    }
+  }
 }
 
 //Ime tvrtke, adresa, oib, telefon, mail
@@ -55,22 +118,74 @@ function SetupModal(props: Props) {
       aria-describedby="simple-modal-description"
     >
       <div className={classes.paper}>
+        <h1>Unesite podatke firme:</h1>
+        <div className={classes.wrapper}>
+        <div className={classes.inputRowWrapper}>
         <div className={classes.textfieldWrapper}>
           <Controller
+            rules={rules.CompanyName}
             as={TextField}
             name="CompanyName"
-            label="Ime Tvrtke"
-            rules={{
-              maxLength: {
-                value: 10,
-                message: 'bla bla 10'
-              }
-            }}
+            label="Ime tvrtke"
             control={control}
             defaultValue=""
           />
           {renderErrorForField(errors, 'CompanyName')}
         </div>
+        <div className={classes.textfieldWrapper}>
+          <Controller
+            rules={rules.CompanyAddress}
+            as={TextField}
+            name="CompanyAddress"
+            label="Adresa"
+            control={control}
+            defaultValue=""
+          />
+          {renderErrorForField(errors, 'CompanyAddress')}
+        </div>
+        </div>
+        <div className={classes.inputRowWrapper}>
+        <div className={classes.textfieldWrapper}>
+          <Controller
+            rules={rules.Oib}
+            as={TextField}
+            name="Oib"
+            label="OIB"
+            control={control}
+            defaultValue=""
+          />
+          {renderErrorForField(errors, 'Oib')}
+        </div>
+        <div className={classes.textfieldWrapper}>
+          <Controller
+            rules={rules.Telephone}
+            as={TextField}
+            name="Telephone"
+            label="Broj telefona"
+            control={control}
+            defaultValue=""
+          />
+          {renderErrorForField(errors, 'Telephone')}
+        </div>
+        </div>
+        <div className={classes.inputRowWrapper}>
+        <div className={classes.textfieldWrapperFullWidth}>
+          <Controller
+            rules={rules.Email}
+            className={classes.textfieldWrapperFullWidth}
+            as={TextField}
+            name="Email"
+            label="Email"
+            control={control}
+            defaultValue=""
+          />
+          {renderErrorForField(errors, 'Email')}
+        </div>
+        </div>
+        </div>
+        <Button onClick={handleSubmit((a) => console.log(a))} variant="contained" style={{width: '40%', height: '40px'}} color="primary">
+          Spremi
+        </Button>
       </div>
     </Modal>
   );
