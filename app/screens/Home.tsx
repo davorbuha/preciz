aadbf47ad9356dae278d7d8f49cc48667a89e111
dbuha <dbuha@aaronprojects.de>
2020-06-28 11:37:10 +0200

    home logo

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable global-require */
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable import/no-cycle */
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import useWindowDimensions from '../useDimensions';
-import { RoutesEnum } from '../routes';
+import logo from '../assets/home.png';
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -19,7 +20,7 @@ function HomeScreen() {
   const classes = useStyles();
   return (
     <div className={classes.container} style={{ width, height }}>
-      <p style={{ color: 'red' }}>Hello from HomeScreen</p>
+      <img width="100%" height="100%" src={logo} />
     </div>
   );
 }
