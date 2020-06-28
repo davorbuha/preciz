4f1061c36b67240e73473ceb349d12900b89f09a
dbuha <dbuha@aaronprojects.de>
2020-06-28 19:39:31 +0200

    roboto

--- configs/webpack.config.renderer.prod.babel.js
+++ configs/webpack.config.renderer.prod.babel.js
@@ -27,7 +27,7 @@ export default merge.smart(baseConfig, {
 
   output: {
     path: path.join(__dirname, '..', 'app/dist'),
-    publicPath: './',
+    publicPath: './dist/',
     filename: 'renderer.prod.js'
   },
 
