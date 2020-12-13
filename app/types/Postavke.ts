f48733bd36254fdc210006cac836a037bf98d66a
dbuha <dbuha@aaronprojects.de>
2020-12-13 17:36:31 +0100

    postavke type changed

--- app/types/Postavke.ts
+++ app/types/Postavke.ts
@@ -7,14 +7,26 @@ class Postavke {
   startPostion: number;
   endPosition: number;
   delimiter: boolean;
+  cameraIp: string;
+  cameraUsername: string;
+  cameraPassword: string;
+  cameraChannel: string;
 
   constructor(
     communicationPort: string,
     baudRate: number,
+    cameraIp: string,
+    cameraUsername: string,
+    cameraPassword: string,
+    cameraChannel: string,
     startPosition: number,
     endPostion: number,
     delimiter: boolean
   ) {
+    this.cameraChannel = cameraChannel;
+    this.cameraIp = cameraIp;
+    this.cameraUsername = cameraUsername;
+    this.cameraPassword = cameraPassword;
     this.communicationPort = communicationPort;
     this.baudRate = baudRate;
     this.startPostion = startPosition;
@@ -26,6 +38,10 @@ class Postavke {
     return new Postavke(
       maybe.communication_port,
       maybe.baud_rate,
+      maybe.camera_ip,
+      maybe.camera_username,
+      maybe.camera_password,
+      maybe.camera_channel,
       maybe.start_position,
       maybe.end_position,
       maybe.delimiter ? true : false
@@ -38,7 +54,11 @@ class Postavke {
       baud_rate: this.baudRate,
       start_position: this.startPostion,
       end_position: this.endPosition,
-      delimiter: this.delimiter
+      delimiter: this.delimiter,
+      camera_ip: this.cameraIp,
+      camera_username: this.cameraUsername,
+      camera_password: this.cameraPassword,
+      camera_channel: this.cameraChannel
     };
   }
 }
